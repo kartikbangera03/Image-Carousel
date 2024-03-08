8c54ece7dde2cbc1220b239ead4c7ccbba1e88d7
kartikbangera03 <kartik.bangera.03@gmail.com>
2024-03-08 15:35:32 +0530

    Simple Implementation of Image Carousel, Radio Buttons pending

---
+++ index.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+        const ofset = button.dataset.carouselButton === "prev" ? -1 : 1;
+
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]");
+
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + ofset;
+        if(newIndex < 0 ) newIndex = slides.children.length -1 ;
+        if(newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+
+        // console.log(ofset);
+        // console.log(slides);
+        // console.log(activeSlide);
+        console.log(newIndex);
+    });
+}
+
+);
+
